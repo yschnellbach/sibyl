08920cb944ce0f07d2853fb5f647e1ad78104070
Morgan Askins <maskins@ucdavis.edu>
2019-04-18 16:14:02 -0700

    New histogram class, much faster

--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 Sibyl - A watchman event viewer
 ===============================
+![SibylLogo](/assets/SherlockWM.png)
 
 Why sibyl: They were oracles which gleamed information from Chthonic
 dieties---ie they got their information from within the earth (underground).
@@ -13,32 +14,51 @@ and be cross-platform, with the only strong dependency being on rat-pac.
 Dependencies
 ------------
 ### Required
-  Python3
-  PyQt5
-  PyQtGraph
-  python-opengl
-  rat-pac (compiled with ROOT-6)
+- Python3
+- PyQt5
+- PyQtGraph
+- python-opengl
+- rat-pac (compiled with ROOT-6)
 ### Optional
 - Plotly -- Useful for a web conversion, not currently implemented.
 - SnakeViz is nice for visualizing cProfile
 
 TODO
 ----
-- Setup bonsai library for just-in-time fitting.
-- Factorize the code
-- Split module view
-- File menu: Quit button.
-  - Quit button
-  - Load data file
-- Save state as json
-- Datastream simulator & datastream reading
-  - Define a speed requirement. Maybe 4-10Hz at full occupancy.
-- Geometry helper
-  - Helps the flatmap view know where to make PMT cuts
-  - Could be used to draw in Geant4 objects.
-- Option to run as single window widgets.
+[] Setup bonsai library for just-in-time fitting.
+  [] libBonsai.so
+  [] Bonsai in python
+  [] Fit button (print to screen)
+  [] Fit, print to QT textdocument
+  [] Draw cherenkov ring
+[] Factorize the code
+  [] Make geometry independent
+  [] Include geometry files
+  [] Module base class
+[] Colors
+  [x] Make color choice global.
+  [] Choose from MPL code in context menu
+[] Context menus
+  [] Test different types for each module
+[] Split module view
+[] File menu: Quit button.
+  [] Quit button
+  [] Load data file
+[] Save state as json
+[] Datastream simulator & datastream reading
+  [] Define a speed requirement. Maybe 4-10Hz at full occupancy.
+[] Geometry helper
+  [] Helps the flatmap view know where to make PMT cuts
+  [] Could be used to draw in Geant4 objects.
+[] Histrogram
+  [] Axis labels
+  [] Swap colors
+  [] Control global colors
+  [] Axis ticks
+  [] Axis tick numbers
+[] Option to run as single window widgets.
 
-- Determine if I can drop the pyqtgraph dependency in favor of
+[] Determine if I can drop the pyqtgraph dependency in favor of
 just python-opengl
 
 
--- sibyl_python/SibylHistogram.py
+++ sibyl_python/SibylHistogram.py
@@ -1,123 +1,251 @@
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Navbar
-from matplotlib.figure import Figure
-import matplotlib.cm as cm
-import matplotlib.pyplot as plt
+from PyQt5 import QtGui
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl
 import numpy as np
+import matplotlib.cm as cm
 
-class SibylHistogram(FigureCanvas):
+class SibylHistogram(gl.GLViewWidget):
     '''
-    Generic histogram object (widget). Designed primarily
-    to draw charge/time histograms for an event with a colored
-    axis.
+    SibylHistogram is a GLViewWidget (3D viewport), which uses the GLMeshItem
+    to draw the vertices and faces of the histogram in different colors.
     '''
-
-    press = False
-    bins  = 20
-
-    def __init__(self, parent=None):
-        self._parent = parent
-        self._setup_painter()
-        self.fig.canvas.mpl_connect('button_press_event', self.on_press)
-        self.fig.canvas.mpl_connect('button_release_event', self.on_release)
-        self.fig.canvas.mpl_connect('motion_notify_event', self.on_move)
-
-    def _setup_painter(self):
-        self.fig = Figure()
-        self.fig.patch.set_facecolor('black')
-        self.axes = self.fig.add_subplot(111)
-        self.axes.set_facecolor('black')
-        axcolor = 'red'
-        self.axes.set_xlim(left=0)
-        self.axes.set_ylim(bottom=0)
-        self.axes.spines['bottom'].set_color(axcolor)
-        self.axes.spines['left'].set_color(axcolor)
-        self.axes.tick_params(axis='x', color=axcolor, which='both')
-        self.axes.tick_params(axis='y', color=axcolor, which='both')
-        self.axes.xaxis.label.set_color(axcolor)
-        #self.cbar = self.fig.add_axes([0.85, 0.1, 0.05, 0.8])
-        FigureCanvas.__init__(self, self.fig)
-
-    def on_press(self, event):
-        #print('you pressed', event.button, event.xdata, event.ydata)
-        if event.dblclick:
-            y, x = np.histogram(self.data, self.bins)
-            self.axes.set_ylim(top=1.1*y.max())
-            self.axes.set_xlim(right=x.max())
-            self.redraw()
-            return
-        if event.button == 3: #right click
-            if self.axes.get_yscale() == 'linear': 
-                self.axes.set_ylim(bottom=1e-1)
-                self.axes.set_yscale('log')
-                self.draw()
+    App = None
+    data  = 0   # Input data to be histogrammed
+    xmin  = 0
+    xmax  = 5.5
+    nbins = 20 # Default binning
+    txt   = '( )'
+    zoom_y = 1
+    zoom_x = 1
+    autoMode = True
+    def __init__(self, app=None, parent=None):
+        if self.App is None:
+            if app is not None:
+                self.App = app
             else:
-                self.axes.set_yscale('linear')
-                self.axes.set_ylim(bottom=0)
-                self.draw()
-        if event.button == 1:
-            self._prev_pos = event.xdata, event.ydata
-            self._prev_xmax = self.axes.get_xlim()[1]
-            self._prev_ymax = self.axes.get_ylim()[1]
-            self.press=False
+                self.App = QtGui.QApplication([])
+        super(SibylHistogram,self).__init__()
+        self._parent=parent
+        self._init_camera()
+        self._axes()
+        self._menu()
+        self.setMouseTracking(True)
+        #self.setAutoFillBackground(False)
+        self.histMesh = gl.GLMeshItem(smooth=False)
+        self.addItem(self.histMesh)
+        self.update()
 
-    def on_release(self, event):
-        #print('you pressed', event.button, event.xdata, event.ydata)
-        if event.button == 1:
-            self.press=False
-            dx = self._prev_pos[0] - event.xdata
-            dy = self._prev_pos[1] - event.ydata
-            #print(dx, dy)
+    def _axes(self):
+        self.ax = gl.GLGridItem()
+        self.ax.setSize(5.5,10,1)
+        self.ax.setSpacing(5.5/5, 1, 1)
+        self.ax.translate(5.5/2,10/2.,0)
+        #self.ax.setSize(x=10)
+        #self.ax = gl.GLBoxItem(glOptions='opaque')
+        #self.ax.setSize(5.5, 0, 1)
+        self.addItem(self.ax)
 
+    def _menu(self):
+        self.menu = QMenu()
+        # Reset (like auto)
+        resetAction = self.menu.addAction("Reset")
+        resetAction.triggered.connect(self._autoset)
+        # Set auto-mode
+        autoModeAction = self.menu.addAction("Auto-coordinates")
+        autoModeAction.setCheckable(True)
+        if self.autoMode:
+            autoModeAction.setChecked(True)
+        autoModeAction.triggered.connect(self._toggle_auto)
 
-    def on_move(self, event):
-        if event.button == 1:
-            dx = event.xdata - self._prev_pos[0]
-            dy = event.ydata - self._prev_pos[1]
-            self.axes.set_xlim(right=self._prev_xmax-dx)
-            self.axes.set_ylim(top=self._prev_ymax-dy)
-            self.redraw()
-        #print('you pressed', event.button, event.xdata, event.ydata)
+    def _toggle_auto(self):
+        self.autoMode = not self.autoMode
 
-    def setData(self, dataset):
-        self.data = dataset[dataset>0]
-        self.redraw()
+    def _autoset(self):
+        # Set best x and y (x to edge, y at 90%)
+        x, y = self.screenToWorld( (0, self.height()*0.1) )
+        end_x = np.max(self.data)
+        self.zoom_x = self.xmax/end_x
+        self.zoom_x = np.max([0, self.zoom_x])
+        self.resetHist()
+        self.zoom_y = y/np.max(self.hy)
+        self.resetMesh()
 
-    def redraw(self):
-        if hasattr(self, 'patches'):
-            t = [b.remove() for b in self.patches]
-        newxlim = self.axes.get_xlim()[1]
-        self._parent.parameters['histXMax'] = newxlim
-        bins = np.linspace(0, newxlim, self.bins)
-        y, x, self.patches = self.axes.hist(
-                np.clip(self.data, bins[0], bins[-1]), 
-                bins=bins)
-        colors = cm.jet(np.linspace(0,1, self.bins))
-        for ptc, clr in zip(self.patches, colors):
-            ptc.set_facecolor(clr)
-        self.draw()
+    def update(self):
+        super(SibylHistogram,self).update()
+
+    def setData(self, data):
+        self.data = data
+        if self.autoMode:
+            self._autoset()
+        else:
+            self.resetHist()
+            self.resetMesh()
+
+    def resetHist(self):
+        hy, hx = np.histogram(self.data, 
+                    bins=np.linspace(self.xmin,self.xmax/self.zoom_x,self.nbins), 
+                    density=False)
+        self.hx = hx
+        self.hy = hy
+
+    def resetMesh(self):
+        vtx, colrs, faces = self._verts(self.hx*self.zoom_x, self.hy*self.zoom_y)
+        self.histMesh.setMeshData(vertexes=vtx, faces=faces, faceColors=colrs)
+        self.histMesh.update()
+        self._parent.parameters['histXMax'] = self.xmax/self.zoom_x
         if self._parent is not None:
             self._parent.drawColors()
 
-    def colorline(self, x, y, z=None, cmap=cm.jet,
-            norm=plt.Normalize(0, 1), linewidth=3, alpha=1.0):
-        if z is None:
-            z = np.linspace(0, 1.0, (len(x)-1)*4)
-        if not hasattr(z, "__iter__"):
-            z = np.array([z])
-        z = np.asarray(z)
-        segments = self.make_segments(x, y)
-        lc = mcoll.LineCollection(segments, array=z, cmap=cmap,
-                norm=norm, linewidth=linewidth, alpha=alpha)
-        return lc
+    def _verts(self, x, y):
+        # points, N bins -> 3*(N-2)+7
+        # for x, [0,0,1,1,1, ..., N, N]
+        nbins = len(y)
+        x_verts     = np.repeat(x, 3)[1:-1]
+        temp_y      = np.repeat(y, 3)
+        temp_y[::3] = 0
+        y_verts     = np.concatenate([temp_y, [0]])
+        z_verts     = np.zeros(x_verts.shape)
+        pos = np.array([x_verts, y_verts, z_verts]).T
+        # Color stuff
+        #clrs = np.repeat(cm.jet(x[:-1]), 2, axis=0)
+        #clrs = cm.cividis(np.linspace(0,1,nbins))
+        clrs = self._parent.parameters['colorMap'](np.linspace(0, 1, nbins))
+        # Faces
+        k,j = np.arange(nbins*3), np.arange(nbins*3)
+        face1 = k.reshape(nbins,3)
+        k[1::3] += 2
+        face2 = j.reshape(nbins,3)
+    
+        face = np.concatenate([face1, face2])
+        clrs = np.concatenate([clrs, clrs])
+        return pos, clrs, face
 
-    def make_segments(self,x, y):
-        rx = np.repeat(x, 4)[2:-2]
-        ry = np.roll(np.vstack((np.zeros(y.shape),y)).ravel('F').repeat(2), -1)[4:]
-        points = np.array([rx, ry]).T.reshape(-1, 1, 2)
-        return np.concatenate([points[:-1], points[1:]], axis=1)
+    def _init_camera(self):
+        # init camera -- this is pseudo 2D
+        self.opts['center'] = QVector3D(0, 0, 0)
+        self.setCameraPosition(pos=np.array([0, 0, 0]))
+        self.opts['elevation'] = 90
+        self.opts['distance']  = 350
+        self.opts['fov']       = 1
+        self.opts['azimuth']   = -90
 
-def histogram(events, **kwargs):
-    y, x = np.histogram(events, **kwargs)
-    kx = ((x+np.roll(x, -1))/2.)[:-1]
-    return kx, y
+    def paintGL(self, *args, **kwargs):
+        #self.paintEvent(*args, **kwargs)
+        super(SibylHistogram,self).paintGL(*args, **kwargs)
+        self.drawLegend()
+    #    pass
+
+    def paintEvent(self, event, *args, **kwargs):
+        #super(SibylHistogram,self).paintGL(*args, **kwargs)
+        super(SibylHistogram,self).paintEvent(event, *args, **kwargs)
+        #self.drawLegend()
+
+
+    def drawLegend(self):
+        margin = 11
+        padding = 6
+        self.qglColor(Qt.white)
+        self.renderText(0.8*self.width(), 0.1*self.height(), self.txt)
+
+    '''
+    Interactive events: mouse, keys, wheel, resize
+    '''
+
+    def contextMenuEvent(self, event):
+        self.menu.exec_(self.mapToGlobal(event.pos()))
+
+    def resizeEvent(self, event):
+        self.plotToBottom()
+        if self.autoMode:
+            self._autoset()
+
+    def wheelEvent(self, event):
+        delta = event.angleDelta().x()
+        if delta == 0:
+            delta = event.angleDelta().y()
+        # Change binning with shift-scroll
+        if event.modifiers() & Qt.ShiftModifier:
+            self.nbins += delta/20.0
+            self.nbins = int(np.max([4, self.nbins]))
+            self.resetHist()
+        # Pan x-axis with fixed nbins (change xmax)
+        elif event.modifiers() & Qt.ControlModifier:
+            self.zoom_x *= 1.001**delta
+            self.resetHist()
+        # Zoom along y-axis
+        else:
+            self.zoom_y *= 1.001**delta
+        self.resetMesh()
+
+    def mouseDoubleClickEvent(self, event):
+        if event.button() == 1:
+            self._autoset()
+
+
+    def mousePressEvent(self, event):
+        pass
+
+    def mouseMoveEvent(self, event):
+        mousePos = event.pos()
+        x, y = mousePos.x(), mousePos.y()
+        mx, my = self.screenToWorld( (x, y) )
+        my /= self.zoom_y
+        mx /= self.zoom_x
+        self.txt = '(%0.2f, %0.2f)'%(mx, my)
+        self.update()
+
+    def leaveEvent(self, event):
+        super(SibylHistogram,self).leaveEvent(event)
+        self.txt = '()'
+        self.update()
+
+    def plotToBottom(self):
+        self.opts['center'] = QVector3D(0, 0, 0)
+        bx, by = self.screenToWorld( (0, self.height() ) )
+        self.opts['center'] = QVector3D(-bx*0.9, -by*0.90, 0)
+
+    def worldToScreen(self, world):
+        # world is a 4D vector, (x,y,z,w)
+        pass
+
+    def screenToWorld(self, screen):
+        mx, my = screen
+        view_w = self.width()
+        view_h = self.height()
+        x = 2.0 * mx / view_w - 1.0
+        y = 1.0 - (2.0 * my / view_h)
+        PMi = self.projectionMatrix().inverted()[0]
+        VMi = self.viewMatrix().inverted()[0]
+        ray_clip = QVector4D(x, y, -1.0, 1.0)
+        ray_eye = PMi * ray_clip
+        ray_eye.setW(0)
+        # Convert to world coordinates
+        ray_world = VMi * ray_eye
+        ray_world = QVector3D(ray_world.x(), ray_world.y(), ray_world.z())
+        ray_world.normalize()
+        origin = np.matrix(self.cameraPosition())
+        origin = self.cameraPosition()
+        # intercept with XY-plane
+        normal = QVector3D(0, 0, 1)
+        pzero = QVector3D(0, 0, 0)
+        d = self.dot( (pzero - origin), normal ) / self.dot(ray_world, normal)
+        intercept = d*ray_world + origin
+        return intercept.x(), intercept.y()
+
+    def dot(self, v1, v2):
+        return QVector3D.dotProduct(v1, v2)
+
+
+
+if __name__ == '__main__':
+    import sys
+    app = QtGui.QApplication([])
+    w = SibylHistogram()
+    w.show()
+    
+    data = np.random.normal(0, 1, 100000)
+    w.setData(data)
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
